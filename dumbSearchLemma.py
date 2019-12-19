17990b9cd81bb86adbe9f311b4a4bfe6d629dfe8
chrisdrymon <p4{u3QCEpH1nd7kg',b6>
2019-12-19 09:18:05 -0600

    Lemma will lemmatize the dumb search.

--- dumbSearchLemma.py
+++ dumbSearchLemma.py
@@ -2,7 +2,7 @@ import os
 from utility import deaccent
 from bs4 import BeautifulSoup
 from collections import Counter
-import pandas
+from cltk.lemmatize.greek.backoff import BackoffGreekLemmatizer
 
 folderPath = os.path.join(os.environ['HOME'], 'Google Drive', 'Greek Texts', 'Plain Text', 'Perseus and OGL',
                           '1.1 No Notes Index or Latin')
@@ -11,6 +11,7 @@ indir = os.listdir(folderPath)
 fileCount = 0
 wordCount = 0
 wordList = Counter()
+lemmatizer = BackoffGreekLemmatizer()
 
 for file in indir:
     if file[-4:] == '.xml':
@@ -19,11 +20,13 @@ for file in indir:
         openText = BeautifulSoup(perseusText, 'lxml-xml')
         for paragraph in openText.find_all('p'):
             i = 0
-            for word in paragraph:
+            splitParagraph = paragraph.text.split()
+            lemmaGraph = lemmatizer.lemmatize(splitParagraph)
+            for word in splitParagraph:
                 if deaccent(word) == 'αντι':
                     try:
                         # gotta fix everything from here on to match the lemmatized paragraph
-                        nextWord = paragraph[i+1]
+                        nextWord = splitParagraph[i+1]
                     except IndexError:
                         nextWord = 'none'
                     print(word, nextWord)
--- testing.py
+++ testing.py
@@ -1,35 +1,7 @@
-import os
-from bs4 import BeautifulSoup
+from cltk.lemmatize.greek.backoff import BackoffGreekLemmatizer
 
-folderPath = os.path.join(os.environ['HOME'], 'Google Drive', 'Greek Texts', 'Plain Text',
-                          'OpenGreekAndLatin-First1KGreek-0e92640', '1.1 No Notes')
-os.chdir(folderPath)
-indir = os.listdir(folderPath)
-fileCount = 1
+sentence = 'κατέβην χθὲς εἰς Πειραιᾶ μετὰ Γλαύκωνος τοῦ Ἀρίστωνος'.split()
 
-for file in indir:
-    noteCount = 0
-    greekCount = 0
-    if file[-4:] == '.xml':
-        print(fileCount, file)
-        greekFile = open(file, 'r', encoding='utf-8')
-        greekText = BeautifulSoup(greekFile, 'lxml')
-        if greekText.author:
-            author = greekText.author.text
-        else:
-            author = 'Unknown'
-        if greekText.title.text == 'Machine readable text':
-            title = greekText.find_all('title')[1].text
-        else:
-            title = greekText.title.text
-        print(author, title)
-        for paragraphs in greekText.find_all('note'):
-            paragraphs.decompose()
-            noteCount += 1
-        fileCount += 1
-        print(noteCount, 'notes extracted.')
-        greekFile.close()
-        with open(file, 'w') as writefile:
-            writefile.write(str(greekText))
+lemmatizer = BackoffGreekLemmatizer()
 
-print(fileCount-1, 'files in', folderPath)
+print(lemmatizer.lemmatize(sentence)[3])
